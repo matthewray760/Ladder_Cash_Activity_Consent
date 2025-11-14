0555a2a3bfc17a2aa93fb80a415d9aa7d82ae58d
matthewray760 <165307255+matthewray760@users.noreply.github.com>
2025-11-14 14:18:29 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,79 @@
 import pandas as pd
+from load_data import load_mappings, load_cash_activity, load_tran_detail
+from to_excel import def_excel
+from checks import cash_tran_check
 
 
-filename = '10.2025_final'
+to_excel = False
+
+use_excel_for_mapping = True
+
+cashsheet_filename = '10.2025_final'
+mapping_filename = 'GL_Mappings'
+tran_detail_filename = '10.2025_test' ## make sure Amount datatypes is Number
+
+### Load data from cash activity file and mappings file
+cashsheet = load_cash_activity(cashsheet_filename)
+mappings = load_mappings(mapping_filename)
+cash_tran_detail = load_tran_detail(tran_detail_filename)
+
+
+
+### create list of mappings to short description
+modifications = mappings['Short Description'].tolist()
+modifications_man = ['Excess Funds']
+print(modifications)
+
+if use_excel_for_mapping == True:
+    filtered_cashsheet = cashsheet[cashsheet['Short Description'].str.contains('|'.join(modifications))]
+
+else:
+    filtered_cashsheet = cashsheet[cashsheet['Short Description'].str.contains('|'.join(modifications_man))]
+
+
+
+merged_df = pd.merge(filtered_cashsheet,mappings, on='Short Description')
+
+merged_df['Date'] = merged_df['Date'].dt.date
+
+
+
+### Create bulk cash entry
+bulk_cash_entry = merged_df[['Account ID','Date','Amount']].copy()
+
+bulk_cash_entry.loc[bulk_cash_entry['Amount'] > 0, 'Transaction Type'] = 'INC'
+bulk_cash_entry.loc[bulk_cash_entry['Amount'] < 0, 'Transaction Type'] = 'EXP'
+
+bulk_cash_entry.loc[:,'Entry Date'] = bulk_cash_entry['Date']
+bulk_cash_entry.loc[:,'Settle Date'] = bulk_cash_entry['Date']
+bulk_cash_entry.loc[:,'Post Date'] = bulk_cash_entry['Date']
+bulk_cash_entry.loc[:,'Currency'] = 'USD'
+bulk_cash_entry.loc[:,'Asset ID'] = 'CCYUSD'
+
+bulk_cash_entry = bulk_cash_entry[['Account ID','Transaction Type', 'Entry Date', 'Settle Date', 'Post Date', 'Asset ID', 'Currency', 'Amount']]
+
+
+
+#### creating offsetting cash transfers
+
+cash_transfers = bulk_cash_entry.copy()
+cash_transfers['Amount_1'] = cash_transfers['Amount'] * -1
+cash_transfers.drop(columns='Amount', inplace= True)
+cash_transfers.rename(columns={'Amount_1':'Amount'}, inplace=True)
+cash_transfers['Transaction Type'] = 'TRN'
 
 
 
 
-dataframes = []
-
-xls = pd.ExcelFile(fr'C:\Users\matthewray\OneDrive - Clearwater\Desktop\Ladder\Python\Cash Entry File\inputs\{filename}.xlsx')
+concat_df = pd.concat([bulk_cash_entry,cash_transfers])
 
 
-for sheet in xls.sheet_names:
-    if 'Cash Activity' in sheet:
-        df = pd.read_excel(xls, sheet_name=sheet, skiprows=3)
-        df['Source_Sheet'] = sheet
 
-        dataframes.append(df)
+### do Checks
 
+tran_check = cash_tran_check(bulk_entry=bulk_cash_entry, cash_transactions=cash_tran_detail)
 
-combined_df = pd.concat(dataframes, ignore_index=True)
+if to_excel == True:
+    def_excel(cashsheet_filename=cashsheet_filename,bulk_entry=bulk_cash_entry,cash_transfers=cash_transfers,concat_df=concat_df)
 
-combined_df.dropna(subset=['Short Description'], inplace=True)
-
-
-consent_fees = ['Float Payment','Consent','Defeasance']
-
-combined_df = combined_df[combined_df['Short Description'].str.contains('|'.join(consent_fees))]
-
-combined_df.to_excel(fr'C:\Users\matthewray\OneDrive - Clearwater\Desktop\Ladder\Python\Cash Entry File\outputs\{filename}_output.xlsx')
-
-print(combined_df)
+#bulk_cash_entry.to_excel(fr'C:\Users\matthewray\OneDrive - Clearwater\Desktop\Ladder\Python\GL_Entries\outputs\{cashsheet_filename}_output.xlsx')
